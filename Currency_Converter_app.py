c78e53d1ecb60286e5633e7efc18e89f6b189f6c
Namir50 <namirsayyed50@gmail.com>
2024-06-30 16:01:41 +0530

    Changes

--- Currency_Converter_app.py
+++ Currency_Converter_app.py
@@ -3,7 +3,8 @@ import tkinter as tk
 
 window = tk.Tk()
 window.geometry("500x360")
-t1 = tk.Label(window,text = "Currency Converter", font = "Times")
+t1 = tk.Label(window,text = "Currency Converter", font = "Times 20 bold").place(x = 120, y = 30)
+t2 = tk.Label(window, text = "Enter amount: ",font = "Times 10 bold").place(x = 50,y=80)
 window.mainloop()
 # a = CurrencyConverter()
 # print(a.convert(12,"USD","INR"))
