0aa95b4180539e68aeda287c8bfe500d34fd2cb2
Namir50 <namirsayyed50@gmail.com>
2024-06-30 15:50:20 +0530

    Project

---
+++ Currency_Converter_app.py
@@ -0,0 +1,9 @@
+from currency_converter import CurrencyConverter
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry("500x360")
+t1 = tk.Label(window,text = "Currency Converter", font = "Times")
+window.mainloop()
+# a = CurrencyConverter()
+# print(a.convert(12,"USD","INR"))
