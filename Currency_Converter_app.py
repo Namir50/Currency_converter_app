4b1e6430b41d913a1521559b1726ff6f5297fa6c
Namir50 <namirsayyed50@gmail.com>
2024-07-03 01:42:03 +0530

    New changes

--- Currency_Converter_app.py
+++ Currency_Converter_app.py
@@ -12,7 +12,7 @@ def clicked():
     data = (a.convert(am1,cur1,cur2))
     l5 = tk.Label(window,text = data, font = "Times 17 bold").place(x = 120, y = 260)
     
-t1 = tk.Label(window,text = "Currency Converter", font = "Times 21 bold").place(x = 120, y = 30)
+t1 = tk.Label(window,text = "Currency Converter", font = "Times 22 bold").place(x = 120, y = 30)
 
 t2 = tk.Label(window, text = "Enter amount: ",font = "Times 10 bold").place(x = 50,y=80)
 e1 = tk.Entry(window)
